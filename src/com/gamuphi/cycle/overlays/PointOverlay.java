fe7e6ef210a94a1f350214aea0d844490aa63d43
Chris Constantine <chris@heyzap.com>
2011-11-28 14:08:56 -0800

    fix warning

--- src/com/gamuphi/cycle/overlays/PointOverlay.java
+++ src/com/gamuphi/cycle/overlays/PointOverlay.java
@@ -9,7 +9,6 @@ import android.graphics.Point;
 
 import com.gamuphi.cycle.models.LocationFix;
 import com.gamuphi.cycle.utils.LatitudeScaleCache;
-import com.gamuphi.cycle.utils.Logger;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
